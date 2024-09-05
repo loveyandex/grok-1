57629c61bc3d2eb1e4158d5a277897ff4c12e5f9
loveyandex <53381292+loveyandex@users.noreply.github.com>
2024-09-05 10:18:07 +0330

    Update run.py

--- run.py
+++ run.py
@@ -57,7 +57,7 @@ def main():
         name="local",
         load=CKPT_PATH,
         tokenizer_path="./tokenizer.model",
-        local_mesh_config=(1, 8),
+        local_mesh_config=(1, 1),
         between_hosts_config=(1, 1),
     )
     inference_runner.initialize()
